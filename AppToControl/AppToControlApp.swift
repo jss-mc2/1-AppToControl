f623cd553a292a77ba1bccb7a8559384ebfad0af
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-06-12 15:41:05 +0700

    Initial Commit

---
+++ AppToControl/AppToControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppToControlApp.swift
+//  AppToControl
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AppToControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
